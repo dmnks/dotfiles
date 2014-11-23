c624509745354e9726945cf3113dfc1adc0acab7
Michal Domonkos <domonkos.michal@gmail.com>
2014-11-23 13:31:14 +0100

    Install vim to /usr/local

--- bashrc
+++ bashrc
@@ -6,12 +6,4 @@ if [ -f /etc/bashrc ]; then
 fi
 
 # User specific aliases and functions
-
-# Use our custom vim build
-MYVIM="~/software/vim/install"
-alias vim="$MYVIM/bin/vim"
-alias gvim="$MYVIM/bin/gvim"
-alias vimdiff="$MYVIM/bin/vimdiff"
-alias gvimdiff="$MYVIM/bin/gvimdiff"
-export MANPATH="$MYVIM/share/man:$(manpath)"
-export GIT_EDITOR="$MYVIM/bin/gvim -f"
+export GIT_EDITOR="gvim -f"
--- getvim.sh
+++ getvim.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
-# Build the latest vim locally in the $HOME dir
+# Build the latest vim
 
-DIR=~/software/vim
-REPO=$DIR/repo
-INSTALL=$DIR/install
+REPO=$HOME/.vim_source
 
-# if this is the first time this script is being run, install the build deps
-# and download the vim's sources
-if [ ! -d $DIR ]; then
-    mkdir -p $DIR
-    # install build deps
-    sudo yum install -y mercurial gcc ncurses-devel python-devel gtk2-devel \
-        libSM-devel libXt-devel libXpm-devel
-    # download the sources
+# install build deps
+sudo yum install -y mercurial gcc ncurses-devel python-devel gtk2-devel \
+    libSM-devel libXt-devel libXpm-devel
+
+# prepare the sources
+if [ ! -d $REPO ]; then
     hg clone https://vim.googlecode.com/hg/ $REPO
 fi
-
 cd $REPO/src || exit 1
-
-# pull any changes
+sudo make uninstall
+make distclean
 hg pull
 hg update
 
-# clean up
-make distclean
-rm -rf $INSTALL
-
 # compile and install
 ./configure \
-    --prefix=$INSTALL \
     --with-features=huge \
     --enable-pythoninterp \
     --with-compiledby=dmnks &&
-make && make install
+make && sudo make install
