45984c7f2b9d81aa1c8aaeeab2a7b190ce043123
Michal Domonkos <mdomonko@redhat.com>
2015-02-01 22:33:04 +0100

    Install vim using stow

--- getvim.sh
+++ getvim.sh
@@ -2,24 +2,35 @@
 # Build the latest vim
 
 REPO=$HOME/.vim_source
+STOW=/usr/local/stow
 
-# install build deps
+# Install build deps
 sudo yum install -y mercurial gcc ncurses-devel python-devel gtk2-devel \
     libSM-devel libXt-devel libXpm-devel
 
-# prepare the sources
+# Prepare the sources
 if [ ! -d $REPO ]; then
     hg clone https://vim.googlecode.com/hg/ $REPO
 fi
 cd $REPO/src || exit 1
-sudo make uninstall
 make distclean
 hg pull
 hg update
 
-# compile and install
+# Unstow and remove any existing vim
+sudo stow -d $STOW -D vim
+sudo rm -rf $STOW/vim
+
+# Compile and install into the stow directory
 ./configure \
+    --prefix=$STOW/vim \
     --with-features=huge \
     --enable-pythoninterp \
     --with-compiledby=dmnks &&
 make && sudo make install
+
+# Stow it
+sudo stow -d $STOW vim
+
+# Use the new vim right now
+hash -r
