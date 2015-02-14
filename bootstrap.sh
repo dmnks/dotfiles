f5e5610fba273c7c3d63faf60cb6a0bd6c6a9cec
Michal Domonkos <mdomonko@redhat.com>
2015-02-14 12:46:22 +0100

    Clone with submodules too

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo yum install -y git stow python-pip
 sudo pip install pyflakes pep8
 
 # Clone the repo
-git clone https://github.com/dmnks/dotfiles.git ~/.dotfiles
+git clone --recursive https://github.com/dmnks/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 
 # Compile the latest vim
