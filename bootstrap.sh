5cfe5d53d4825f54a611b80dd09cf355d5c356ee
Michal Domonkos <mdomonko@redhat.com>
2016-08-13 17:36:35 +0200

    Minor bootstrap fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,11 +6,10 @@ set -o errexit
 sudo dnf install -y vim git stow python-pip tmux ctags colordiff
 sudo pip install flake8 pudb
 
-# Install the dotfiles
-mv ~/.bashrc ~/.bashrc.orig  # There's a default .bashrc already
-stow -v bash vim git tmux fonts python pulse
+[ -f ~/.bashrc ] && mv ~/.bashrc{,.orig}
+stow -v bash vim git tmux fonts pulse
+stow -v --no-folding python  # Don't fold pudb
 
-# Install Vundle and all plugins
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u ~/.vundle +PluginInstall +qall
 
