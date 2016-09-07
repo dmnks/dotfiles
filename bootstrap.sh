f5f4a4006c6c12e2aa3bd5d70d0f7bc4be488b27
Michal Domonkos <mdomonko@redhat.com>
2016-09-07 17:46:07 +0200

    Add thinbox script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo dnf install -y vim git stow python-pip tmux ctags colordiff tig
 sudo pip install flake8 pudb
 
 [ -f ~/.bashrc ] && mv ~/.bashrc{,.orig}
-stow -v bash vim git tmux fonts pulse
+stow -v bin bash vim git tmux fonts pulse
 stow -v --no-folding python  # Don't fold pudb
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
