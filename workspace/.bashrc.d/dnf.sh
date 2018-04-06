83941291c73643dc7f6bae57d5d02d7e5198a1be
Michal Domonkos <mdomonko@redhat.com>
2018-04-06 19:02:51 +0200

    workspace: dnf: support docker args

--- workspace/.bashrc.d/dnf.sh
+++ workspace/.bashrc.d/dnf.sh
@@ -2,5 +2,5 @@ function ddnf() {
     dnf_path=$HOME/projects/dnf
     libdnf_path=$HOME/projects/libdnf/build/
     docker build -t dnf $HOME/.dockerfile/dnf/
-    docker run -v$dnf_path:/dnf:ro,z -v$libdnf_path:/libdnf:ro,z -it dnf
+    docker run -v$dnf_path:/dnf:ro,z -v$libdnf_path:/libdnf:ro,z -it $@ dnf
 }
