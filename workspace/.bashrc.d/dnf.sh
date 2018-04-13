c01c72aebf15192827c08a7ff7562e28ada847c6
Michal Domonkos <mdomonko@redhat.com>
2018-04-13 13:55:49 +0200

    Improve dnf workspace

--- workspace/.bashrc.d/dnf.sh
+++ workspace/.bashrc.d/dnf.sh
@@ -1,6 +1,16 @@
-function ddnf() {
-    dnf_path=$HOME/projects/dnf
-    libdnf_path=$HOME/projects/libdnf/build/
+DNF_PATH=$HOME/projects/dnf
+LIBDNF_BUILD=$HOME/projects/libdnf/build
+LIBDNF_PATH=$LIBDNF_BUILD/libdnf
+LIBDNF_PYTHON_PATH=$LIBDNF_BUILD/python/hawkey
+
+function dnf-load-env() {
+    export PYTHONPATH=$LIBDNF_PYTHON_PATH:$PYTHONPATH
+    export LD_LIBRARY_PATH=$LIBDNF_PATH:$LD_LIBRARY_PATH
+    export GI_TYPELIB_PATH=$LIBDNF_PATH:$GI_TYPELIB_PATH
+    export PS1="(dnf)$PS1"
+}
+
+function dnf-docker() {
     docker build -t dnf $HOME/.dockerfile/dnf/
-    docker run -v$dnf_path:/dnf:ro,z -v$libdnf_path:/libdnf:ro,z -it $@ dnf
+    docker run -v$DNF_PATH:/dnf:ro,z -v$LIBDNF_BUILD:/libdnf:ro,z -it $@ dnf
 }
