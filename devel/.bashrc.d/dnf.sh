e8e47a2103d60397807636f1ef4392763904290c
Michal Domonkos <mdomonko@redhat.com>
2018-04-27 16:10:57 +0200

    Update dnf dockerfile

--- devel/.bashrc.d/dnf.sh
+++ devel/.bashrc.d/dnf.sh
@@ -1,16 +1,7 @@
 DNF_PATH=$HOME/projects/dnf
-LIBDNF_BUILD=$HOME/projects/libdnf/build
-LIBDNF_PATH=$LIBDNF_BUILD/libdnf
-LIBDNF_PYTHON_PATH=$LIBDNF_BUILD/python/hawkey
-
-function dnf-load-env() {
-    export PYTHONPATH=$LIBDNF_PYTHON_PATH:$PYTHONPATH
-    export LD_LIBRARY_PATH=$LIBDNF_PATH:$LD_LIBRARY_PATH
-    export GI_TYPELIB_PATH=$LIBDNF_PATH:$GI_TYPELIB_PATH
-    export PS1="(dnf)$PS1"
-}
+LIBDNF_PATH=$HOME/projects/libdnf
 
 function dnf-docker() {
     docker build -t dnf -f $HOME/.dockerfile/dnf $HOME/.dockerfile/
-    docker run -v$DNF_PATH:/dnf:ro,z -v$LIBDNF_BUILD:/libdnf:ro,z -it $@ dnf
+    docker run -v$DNF_PATH:/dnf:ro,z -v$LIBDNF_PATH:/libdnf:ro,z -it $@ dnf
 }
