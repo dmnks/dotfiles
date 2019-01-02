76a85cee49a0419811a08d612ec036be158cdefe
Michal Domonkos <mdomonko@redhat.com>
2019-01-02 16:47:01 +0100

    workspace: add dnf plugin support

--- workspace/.bashrc.d/workspace.sh
+++ workspace/.bashrc.d/workspace.sh
@@ -1,8 +1,11 @@
 DNF_PATH=$HOME/code/dnf
+DNF_PLUGINS_PATH=$HOME/code/dnf-plugins-core/plugins
 LIBDNF_PATH=$HOME/code/libdnf
 podman-dnf() {
     podman run \
         --detach-keys="ctrl-@" \
-        -v=$DNF_PATH:/dnf:ro,z -v=$LIBDNF_PATH:/libdnf:ro,z \
+        -v=$DNF_PATH:/dnf:ro,z \
+        -v=$DNF_PLUGINS_PATH:/plugins:ro,z \
+        -v=$LIBDNF_PATH:/libdnf:ro,z \
         -it $@ dnf-workspace
 }
