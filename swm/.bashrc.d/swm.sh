bd68c555a39284ec6ba1c079605226a6b11e1367
Michal Domonkos <mdomonko@redhat.com>
2019-02-20 17:17:16 +0100

    swm: update Dockerfile

--- swm/.bashrc.d/swm.sh
+++ swm/.bashrc.d/swm.sh
@@ -1,11 +1,8 @@
-DNF_PATH=$HOME/code/dnf
-DNF_PLUGINS_PATH=$HOME/code/dnf-plugins-core/plugins
-LIBDNF_PATH=$HOME/code/libdnf
 podman-dnf() {
     podman run \
         --detach-keys="ctrl-@" \
-        -v=$DNF_PATH:/dnf:ro,z \
-        -v=$DNF_PLUGINS_PATH:/plugins:ro,z \
-        -v=$LIBDNF_PATH:/libdnf:ro,z \
+        -e TERM -it \
+        -v=$PWD/dnf:/dnf \
+        -v=$PWD/libdnf:/libdnf \
         -it $@ dnf-devel
 }
