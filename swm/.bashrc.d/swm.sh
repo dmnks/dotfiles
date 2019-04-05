bc013d2d6330eb3c99f3f1bafc33449efa572179
Michal Domonkos <mdomonko@redhat.com>
2019-04-05 19:10:52 +0200

    swm: improve dockerfile

--- swm/.bashrc.d/swm.sh
+++ swm/.bashrc.d/swm.sh
@@ -1,9 +1,11 @@
-podman-dnf() {
-    podman run \
-        --detach-keys="ctrl-@" \
-        -e TERM -it \
-        -v=$PWD/dnf:/dnf \
-        -v=$PWD/libdnf:/libdnf \
-        -v=$PWD/ci-dnf-stack/dnf-behave-tests:/test \
-        -it $@ dnf-devel
+dnf-run() {
+    local bin=$1
+    shift
+    $bin run \
+         --detach-keys="ctrl-@" \
+         -e TERM -it \
+         -v=$PWD/dnf:/dnf \
+         -v=$PWD/libdnf:/libdnf \
+         -v=$PWD/ci-dnf-stack/dnf-behave-tests:/test \
+         -it $@ dnf-devel
 }
