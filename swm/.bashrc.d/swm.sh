75cfdab13c650bd3fc2b0c5744cb12b7dba07b8d
Michal Domonkos <mdomonko@redhat.com>
2019-04-08 20:01:00 +0200

    bashrc: add tmux init scripts

--- swm/.bashrc.d/swm.sh
+++ swm/.bashrc.d/swm.sh
@@ -9,3 +9,23 @@ dnf-run() {
          -v=$PWD/ci-dnf-stack/dnf-behave-tests:/test \
          -it $@ dnf-devel
 }
+
+tmux-init() {
+    case $1 in
+    "utils")
+        tmux new-session -ds "utils" -n "chat"
+        tmux send-keys "weechat" C-m
+        tmux new-window -t "utils" -n "wiki"
+        tmux send-keys "vim -c 'VimwikiIndex'" C-m
+        tmux attach -t "utils"
+        ;;
+    "devel")
+        tmux new-session -ds "devel" -n "code"
+        tmux send-keys "vim" C-m
+        tmux new-window -t "devel" -n "git"
+        tmux send-keys "tig status" C-m
+        tmux new-window -t "devel" -n "build"
+        tmux attach -t "devel"
+        ;;
+    esac
+}
