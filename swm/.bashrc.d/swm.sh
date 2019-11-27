88a838f1d5edfb709228e765462b5e5bee607d4a
Michal Domonkos <mdomonko@redhat.com>
2019-11-27 13:15:45 +0100

    Add tmux-init back

---
+++ swm/.bashrc.d/swm.sh
@@ -0,0 +1,20 @@
+tmux-init() {
+    case $1 in
+    "utils")
+        tmux new-session -ds "utils" -n "chat"
+        tmux send-keys "weechat" C-m
+        tmux new-window -t "utils" -n "wiki"
+        tmux send-keys "vim -c 'VimwikiIndex'" C-m
+        tmux attach -t "utils"
+        ;;
+    "devel")
+        local name=$2
+        tmux new-session -ds "$name" -n "code"
+        tmux send-keys "vim" C-m
+        tmux new-window -t "$name" -n "git"
+        tmux send-keys "tig status" C-m
+        tmux new-window -t "$name" -n "build"
+        tmux attach -t "$name"
+        ;;
+    esac
+}
