d02756866641b7922a3e00bd0767967792a23c8f
Michal Domonkos <mdomonko@redhat.com>
2019-11-27 13:15:53 +0100

    Add scl-utils dockerfile

--- swm/.bashrc.d/swm.sh
+++ swm/.bashrc.d/swm.sh
@@ -1,20 +1,17 @@
-tmux-init() {
-    case $1 in
-    "utils")
-        tmux new-session -ds "utils" -n "chat"
-        tmux send-keys "weechat" C-m
-        tmux new-window -t "utils" -n "wiki"
-        tmux send-keys "vim -c 'VimwikiIndex'" C-m
-        tmux attach -t "utils"
-        ;;
-    "devel")
-        local name=$2
-        tmux new-session -ds "$name" -n "code"
-        tmux send-keys "vim" C-m
-        tmux new-window -t "$name" -n "git"
-        tmux send-keys "tig status" C-m
-        tmux new-window -t "$name" -n "build"
-        tmux attach -t "$name"
-        ;;
-    esac
+tmdev() {
+    NAME=$(basename $PWD)
+    tmux new-session -ds "$NAME" -n "code"
+    tmux send-keys "vim" C-m
+    tmux new-window -t "$NAME" -n "git"
+    tmux send-keys "tig status" C-m
+    tmux new-window -t "$NAME" -n "build"
+    tmux attach -t "$NAME"
+}
+
+tmutil() {
+    tmux new-session -ds "utils" -n "chat"
+    tmux send-keys "weechat" C-m
+    tmux new-window -t "utils" -n "wiki"
+    tmux send-keys "vim -c 'VimwikiIndex'" C-m
+    tmux attach -t "utils"
 }
