201c113cf8f0a0a3c64912807135213583415b7f
Michal Domonkos <mdomonko@redhat.com>
2020-08-17 08:11:31 +0200

    Update tmux helpers

--- swm/.bashrc.d/swm.sh
+++ swm/.bashrc.d/swm.sh
@@ -1,4 +1,4 @@
-tmdev() {
+tmux-ide() {
     NAME=$(basename $PWD)
     tmux new-session -ds "$NAME" -n "code"
     tmux send-keys "vim" C-m
@@ -6,12 +6,4 @@ tmdev() {
     tmux send-keys "tig status" C-m
     tmux new-window -t "$NAME" -n "build"
     tmux attach -t "$NAME"
-}
-
-tmutil() {
-    tmux new-session -ds "utils" -n "chat"
-    tmux send-keys "weechat" C-m
-    tmux new-window -t "utils" -n "wiki"
-    tmux send-keys "vim -c 'VimwikiIndex'" C-m
-    tmux attach -t "utils"
 }
