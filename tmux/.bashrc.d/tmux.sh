0d47b2ba4f8e284d73f63e3d61195c3fb9127a2d
Michal Domonkos <mdomonko@redhat.com>
2022-10-05 14:59:52 +0200

    Update tm()

--- tmux/.bashrc.d/tmux.sh
+++ tmux/.bashrc.d/tmux.sh
@@ -2,10 +2,16 @@ alias tmux="systemd-run --quiet --scope --user tmux"
 
 tm() {
     NAME=$(basename $PWD)
-    tmux new-session -ds "$NAME" -n "code"
+
+    tmux new-session -ds "$NAME" -x 255 -n "code"
     tmux send-keys "vim" C-m
+
     tmux new-window -t "$NAME" -n "git"
+    tmux split-window -h -b -l 161
     tmux send-keys "tig status" C-m
+
     tmux new-window -t "$NAME" -n "build"
+    tmux split-window -h -b -l 161
+
     tmux attach -t "$NAME"
 }
