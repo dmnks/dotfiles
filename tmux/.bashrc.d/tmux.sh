53c06039a2df3ce031734d66233696037d387a0e
Michal Domonkos <mdomonko@redhat.com>
2023-11-19 15:34:07 +0100

    Return to tmux

---
+++ tmux/.bashrc.d/tmux.sh
@@ -0,0 +1,11 @@
+alias tmux="systemd-run --quiet --scope --user tmux"
+
+workspace() {
+    [ $# == 0 ] && set -- $(dirname $PWD | xargs basename)/$(basename $PWD)
+    tmux new-session -ds "$1" -n "code"
+    tmux send-keys "vim" C-m
+    tmux new-window -t "$1" -n "git"
+    tmux send-keys "tig status" C-m
+    tmux new-window -t "$1" -n "build"
+    tmux attach -t "$1"
+}
