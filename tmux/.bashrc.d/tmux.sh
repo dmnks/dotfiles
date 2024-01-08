96b7b186b8f388912a04bd6245740cf4273b87bc
Michal Domonkos <mdomonko@redhat.com>
2024-01-08 19:48:38 +0100

    Update workspace function

--- tmux/.bashrc.d/tmux.sh
+++ tmux/.bashrc.d/tmux.sh
@@ -6,10 +6,13 @@ fi
 
 workspace() {
     [ $# == 0 ] && set -- $(dirname $PWD | xargs basename)/$(basename $PWD)
+    tmux has-session -t "$1" 2>/dev/null && return 0
     tmux new-session -ds "$1" -n "code"
-    tmux send-keys "vim" C-m
+    tmux send-keys -t "$1" "vim" C-m
     tmux new-window -t "$1" -n "git"
-    tmux send-keys "tig status" C-m
+    tmux send-keys -t "$1" "tig status" C-m
     tmux new-window -t "$1" -n "build"
+    tmux new-window -t "$1" -n "debug"
+    tmux select-window -t ${1}:+1
     tmux attach -t "$1"
 }
